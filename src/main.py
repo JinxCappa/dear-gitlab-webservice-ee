6275ce74fddb4966661156e9011b5afd43a442e8
Eddie Garcia <eddiegarcia@fastmail.com>
2025-03-08 20:20:20 -0500

    Added blocked versions

--- src/main.py
+++ src/main.py
@@ -18,7 +18,7 @@ def main():
 
     # match the gitlab version which not inside GitHub tags, the github tags contains gitlab version
     for gitlab_version in gitlab_versions:
-        if gitlab_version not in github_tags and gitlab_version not in ["16.5.5", "16.6.3", "16.7.1"] and int(gitlab_version.split('.')[0]) > 12:
+        if gitlab_version not in github_tags and gitlab_version not in ["16.5.5", "16.6.3", "16.7.1", "17.1.5", "17.2.3", "17.2.6", "17.7.5", "17.8.3"] and int(gitlab_version.split('.')[0]) > 12:
             new_tags.append(gitlab_version)
 
     for tag in new_tags:
